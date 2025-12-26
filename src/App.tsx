fef08fca7cb7e242b5b0a9192ec546160598e679
AOISYNM <pandeypnjn@gmail.com>
2025-12-26 20:17:52 +0545

    made changes in the readme

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { APITester } from "./APITester";
+import "./index.css";
+
+
+export function App() {
+  return (
+   <div></div> 
+  );
+}
+
+export default App;
